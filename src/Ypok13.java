afd8a10879869956871fe89562635546b1a4f209
AkhtamIsPutin <Kuukilovekulpush@gmail.com>
2022-04-27 19:56:54 +0600

    test

--- src/Ypok13.java
+++ src/Ypok13.java
@@ -1,5 +1,5 @@
 public class Ypok13 {
     public static void main(String[] args) {
-        System.out.println("Hello World MotherFuck");
+        System.out.println("Hello World MotherFucker");
     }
 }
